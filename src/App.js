b7d2a11e6dbf176d81b0717fd925a9bc84155790
Ykub-Taras <ykub.taras@gmail.com>
2021-06-22 18:59:16 +0300

    Lesson 01 HomeWork

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Posts from "./components/posts/Posts";
 
 export default function App() {
     return (
         <div>
-
+            <Posts/>
         </div>
     );
 }
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,10 @@
+import Post from "./post/Post.js";
+export default function Posts() {
+
+    console.log(Post)
+    return (
+        <div>
+            <Post/>
+        </div>
+    )
+}
