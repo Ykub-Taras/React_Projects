2d87ccb5b24f0222db98bc01af5471a06433ce31
Ykub-Taras <ykub.taras@gmail.com>
2021-06-23 13:48:57 +0300

    Master + Axios

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+let axiosInstance = axios.create({baseURL: 'https://jsonplaceholder.typicode.com'});
+
+const getUsers = () => axiosInstance.get('/users');
+
+const getUser = (id) => axiosInstance.get('/users/' + id);
+export {getUsers, getUser};
