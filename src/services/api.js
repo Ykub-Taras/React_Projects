e70e74d3a66e2d922d7cbc20d9d3e444490019d7
Ykub-Taras <ykub.taras@gmail.com>
2021-06-24 15:10:09 +0300

    Master. DevDependencies are installed

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 let axiosInstance = axios.create({baseURL: 'https://jsonplaceholder.typicode.com'});
 
-const getUsers = () => axiosInstance.get('/users');
+const getUsers = () => axiosInstance('/users');
 
-const getUser = (id) => axiosInstance.get('/users/' + id);
+const getUser = (id) => axiosInstance('/users/' + id);
+
 export {getUsers, getUser};
