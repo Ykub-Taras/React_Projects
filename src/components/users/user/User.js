6a92b8dfa8f857d8af0bb64c3fb57af9882ff095
Ykub-Taras <ykub.taras@gmail.com>
2021-07-01 09:23:46 +0300

    Practice 02 Style changes via useState

--- src/components/users/user/User.js
+++ src/components/users/user/User.js
@@ -1,16 +1,36 @@
+import {useState} from "react";
+
 export default function User({userDataBlock: {id, name, username}, postsInfo}) {
+    const [x, setX] = useState(true);
+    const cls = x ? 'block' : 'none';
     return (
-        <div>
+
+        <div className={'xxx'} style={{display: cls}}>
             <h2>
                 {name}, # {id} ({username})
                 <br/>
                 <button onClick={() => {
                     postsInfo(id);
-                    localStorage.setItem("stylePosts", JSON.stringify('block'));
-                    localStorage.setItem("styleComments", JSON.stringify('none'));
-                }}>View all posts of user
+                  }}>View all posts of user
                 </button>
             </h2>
         </div>
     );
 }
+//----------------------OLD via localStorage -----------------------------------
+// export default function User({userDataBlock: {id, name, username}, postsInfo}) {
+//     return (
+//         <div>
+//             <h2>
+//                 {name}, # {id} ({username})
+//                 <br/>
+//                 <button onClick={() => {
+//                     postsInfo(id);
+//                     localStorage.setItem("stylePosts", JSON.stringify('block'));
+//                     localStorage.setItem("styleComments", JSON.stringify('none'));
+//                 }}>View all posts of user
+//                 </button>
+//             </h2>
+//         </div>
+//     );
+// }
