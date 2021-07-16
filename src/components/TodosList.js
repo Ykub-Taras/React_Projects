a8892d4d161fd956a5a1cb923a6609ffc475f5c2
Ykub-Taras <ykub.taras@gmail.com>
2021-07-16 21:49:15 +0300

    Lesson 06 Changed

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,11 @@
 import React, {Fragment} from "react";
 import {deleteTodoByID, getTodos} from "../services/api";
 import {useDispatch, useSelector} from "react-redux";
+import {addTodos} from "../redux/actionsCreatores";
 
 const Todos = () => {
     const dispatch = useDispatch();
     const todos = useSelector(({todosReducer}) => todosReducer.todos);
-
     return (
         <div>
             {todos.map(todo => (
@@ -15,7 +15,14 @@ const Todos = () => {
                     <h6>ID: <i>{todo.id}</i></h6>
                     <p>Created At: {new Date(todo.createdAt).toDateString()}</p>
                     <p>Status {todo.completed.toString()}</p>
-                    <button onClick={() => deleteTodoByID(todo.id)}>DELETE</button>
+
+                        <button onClick={() => (deleteTodoByID(todo.id),
+                            getTodos().then(value => todos = value.data
+                            ))}>
+                        
+
+
+                        DELETE</button>
                     <hr/>
                 </Fragment>
             ))}
